1653f2accd81d6d4b9e5f59d24ec15ca20c13104
Rahul12189 <81671445+Rahul12189@users.noreply.github.com>
2021-04-06 17:27:57 +0530

    Update views.py

--- WPL_P2/views.py
+++ WPL_P2/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello...This is Jayesh Wani From \'T3\' Batch and My PRN is 1841057")
+    return HttpResponse(" Jayesh Wani 1841057")
